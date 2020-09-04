fb1f025247842b076cb5076eab15b08ca761503a
Jay <mgmediajay@gmail.com>
2020-09-04 15:27:46 +0500

    adding subscribe

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -198,15 +198,15 @@
                                             <div class="widget-short_desc">
                                                 <p>@lang("Subscribe to our newsletters now and stay up-to-date with new collections")</p>
                                             </div>
-                                            <div class="newsletter-form_wrap">
+                                            <div class="newsletter-form_wrap" id="mc_embed_signup">
                                                 <form action="https://uzbekmart.us17.list-manage.com/subscribe/post?u=2b6a4310f302a46f5382b290f&amp;id=c642c400d8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="newsletters-form validate" target="_blank" novalidate>
                                                     <div id="mc_embed_signup_scroll">
-                                                        {{-- <div id="mc-form" class="mc-form subscribe-form">
-                                                            <input id="mc-email" class="newsletter-input" type="email" autocomplete="off" placeholder="E-mail qoldiring" />
-                                                            <button class="newsletter-btn" id="mc-submit">
+                                                        <div id="mc-form" class="mc-form subscribe-form">
+                                                            <input type="email" value="" name="EMAIL" class="newsletter-input email" id="mce-EMAIL" autocomplete="off" placeholder="E-mail qoldiring" />
+                                                            <div class="clear"><button name="subscribe" id="mc-embedded-subscribe" class="newsletter-btn" id="mc-submit">
                                                                 <i class="ion-android-mail" aria-hidden="true"></i>
-                                                            </button>
-                                                        </div> --}}
+                                                            </button></div>
+                                                        </div>
                                                     </div>
                                                 </form>
                                             </div>
