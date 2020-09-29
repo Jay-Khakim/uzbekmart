243d9e6da124f7457f105d050b875e64d238ca6e
Jay-Khakim <mgmediajay@gmail.com>
2020-09-29 17:35:51 +0500

    partners page

--- resources/views/inc/partners.blade.php
+++ resources/views/inc/partners.blade.php
@@ -122,3 +122,128 @@
     </div>
 </div><br>
 <!-- Hiraola's Product Tab Area TwoEnd Here -->
+
+<!-- Begin Hiraola's Product Tab Area Two -->
+        <div class="hiraola-product-tab_area-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="product-tab">
+                            <div class="hiraola-tab_title">
+                                <h4>@lang("Foreign partners")</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                        <div class="hiraola-product_slider">
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="https://www.asianafrican.org/">
+                                            <img class="primary-img" src="/assets/images/fpartners/fp1.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" style="padding-bottom: 28px;">
+                                            <h6 align="center"><a class="product-name" href="https://www.asianafrican.org/">@lang("Asian-African Chamber of Commerce & Industry (AACCI)")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="https://rcci.org.pk/">
+                                            <img class="primary-img" src="/assets/images/fpartners/fp2.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" style="padding-bottom: 40px;">
+                                            <h6 align="center"><a class="product-name" href="https://rcci.org.pk/">@lang("Rawalpindi Chamber of Commerce and Industry (RCCI)")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="http://www.ascame.org/">
+                                            <img class="primary-img" src="/assets/images/fpartners/fp3.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" style="padding-bottom: 10px;">
+                                            <h6 align="center"><a class="product-name" href="http://www.ascame.org/">@lang("The Association of the Mediterranean Chambers of Commerce and Industry (ASCAME)")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item"  >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="https://www.micci.com/Index.aspx">
+                                            <img class="primary-img" src="/assets/images/fpartners/fp4.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" style="padding-bottom: 10px;">
+                                            <h6 align="center"><a class="product-name" href="https://www.micci.com/Index.aspx">@lang("Malaysian International Chamber of Commerce & Industry(MICCI)")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="https://www.indianimporterschambers.com/">
+                                            <img class="primary-img" src="/assets/images/fpartners/fp5.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info pt-2" style="padding-bottom: 15px;">
+                                            <h6 align="center"><a class="product-name" href="https://www.indianimporterschambers.com/">@lang("Indian Importers Chamber of Commerce and Industry(IICCI)")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="https://bccci-bd.org/">
+                                            <img class="primary-img" src="/assets/images/fpartners/fp6.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info pt-1" style="padding-bottom: 20px;">
+                                            <h6 align="center"><a class="product-name" href="https://bccci-bd.org/">@lang("Bangladesh China Chamber of Commerce and Industr(BCCCI)")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+
+                        
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <!-- Hiraola's Product Tab Area TwoEnd Here -->
