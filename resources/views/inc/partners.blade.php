ddf565a226cc86d5800e0c975b75bbfcc72f0d12
Jay-Khakim <mgmediajay@gmail.com>
2020-10-13 13:01:11 +0500

    updates in partners page

--- resources/views/inc/partners.blade.php
+++ resources/views/inc/partners.blade.php
@@ -1,4 +1,179 @@
+
 <!-- Begin Hiraola's Product Tab Area Two -->
+        <div class="hiraola-product-tab_area-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="product-tab">
+                            <div class="hiraola-tab_title">
+                                <h4>@lang("Embassies of Uzbekistan in abroad")</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                        <div class="hiraola-product_slider">
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/1.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" >
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">
+                                                @lang("Embassy of Uzbekistan in India")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/2.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" >
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in Azerbaijan")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/3.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" >
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in Kazakhstan")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+
+
+                            <div class="slide-item"  >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/4.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info" >
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in Pakistan")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/5.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info pt-2">
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in Russia")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/6.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info pt-1">
+                                            <h6 align="center"><a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in the United States")</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/7.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info pt-1">
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in Turkey")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+                            <!-- Begin Hiraola's Slide Item Area -->
+                            <div class="slide-item" >
+                                <div class="single_product" >
+                                    <div class="product-img">
+                                        <a href="javascript:void(0)">
+                                            <img class="primary-img" src="/assets/images/flag/8.jpg" alt="Hiraola's Product Image">
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="hiraola-product_content">
+                                        <div class="product-desc_info pt-1">
+                                            <h6 align="center">
+                                                <a class="product-name" href="javascript:void(0)">@lang("Embassy of Uzbekistan in Ukraine")</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Hiraola's Slide Item Area End Here -->
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <!-- Hiraola's Product Tab Area TwoEnd Here -->
+
+{{-- <!-- Begin Hiraola's Product Tab Area Two -->
 <div class="hiraola-product-tab_area-3">
     <div class="container">
         <div class="row">
@@ -113,7 +288,7 @@
         </div>
     </div>
 </div><br>
-<!-- Hiraola's Product Tab Area TwoEnd Here -->
+<!-- Hiraola's Product Tab Area TwoEnd Here --> --}}
 
 <!-- Begin Hiraola's Product Tab Area Two -->
         <div class="hiraola-product-tab_area-3">
